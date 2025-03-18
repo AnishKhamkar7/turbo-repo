1ac779e59121cac19f8f6458723b233d3eb81791
Anish <anishkhamkar7@gmail.com>
2025-03-18 09:35:56 +0530

    feat: added post endpoint for adding user in express api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { prismaClient } from "db/client";
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const data = await prismaClient.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  res.json({
+    id: data.id,
+    username: data.username,
+    password: data.password,
+  });
+});
 
 app.listen(9000, () => {
   console.log("Server Running on 9000");
